3b38e46750b5e9b409cc841b8cba8dc888829907
RBing123 <ryan3552215@gmail.com>
2024-09-26 22:52:59 +0800

    add assembly code prototype and test data

---
+++ hw.s
@@ -0,0 +1,13 @@
+.data
+    test_data_1: .dword 0xFFFFFFFF, 0x00000000
+    test_data_2: .dword 0x7FF00132, 0xCAE65324
+    test_data_3: .dword 0x00028E20, 0x00001843
+    print_string: .string
+
+.text
+my_clz:
+
+hamming_distance:
+
+main:
+
