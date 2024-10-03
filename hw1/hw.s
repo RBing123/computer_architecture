ef26483a87ab1cf93e15e2b23bdc51869729a833
RBing123 <ryan3552215@gmail.com>
2024-10-03 14:35:35 +0800

    lw/sw and call my_clz

--- hw1/hw.s
+++ hw1/hw.s
@@ -17,40 +17,54 @@ main:
     sw t2, 8(sp)
 
     # initialize main loop
-    addi s0, zero, 3 # number of test cases
-    addi s1, zero, 1 # count of test case
-    addi s2, sp, 0 # point to test_data_1
+    addi s0, zero, 3        # number of test cases
+    addi s1, zero, 1        # count of test case
+    addi s2, sp, 0          # point to test_data_1
     
+    # calculate hamming distance for each pair of data
+    lw a0, 0(sp)            # load first number
+    lw a1, 4(sp)            # load second number
+    jal ra, hamming_distance
+
+    lw a0, 4(sp)            # load first number of next pair
+    lw a1, 8(sp)            # load second number
+    jal ra, hamming_distance
+
+    lw a0, 8(sp)            # load first number of the third pair
+    lw a1, 12(sp)           # load second number
+    jal ra, hamming_distance
+
+# hamming distance
+hamming_distance:
+    addi sp, sp, -4         # Allocate memory for hamming_count on stack
+    xor t0, a0, a1          # diff = x ^ y
+    addi t1, t1, 0          # hamming_count = 0
+    mv a0, t0               # move data to a0 and call my_clz
+    jal ra, my_clz          # jump and link to my_clz
+    
+
 # clz function
 my_clz:
-    addi sp, sp, -8      # return count and hold 1U
-    sw ra, 0(sp)         # save return address
-    mv s2, a0            # move input argument to saved register
-    addi t0, zero, 31    # init i=31
-    addi s0, zero, 1     # hold 1U
-    addi s1, zero, 0     # init count = 0
+    addi sp, sp, -8         # return count and hold 1U
+    sw ra, 0(sp)            # save return address
+    mv s2, a0               # move input argument to saved register
+    addi t0, zero, 31       # init i=31
+    addi s0, zero, 1        # hold 1U
+    addi s1, zero, 0        # init count = 0
     bgez t0, my_clz_loop
 
 my_clz_loop:
-    sll t1, s0, t0       # (1U << i)
-    and a1, s2, t1       # x & (1U << i)
+    sll t1, s0, t0          # (1U << i)
+    and a1, s2, t1          # x & (1U << i)
     
     beqz t2, clz_finish
-    addi s1, s1, 1       # count++
-    addi t0, t0, -1      # i--
+    addi s1, s1, 1          # count++
+    addi t0, t0, -1         # i--
     bgez t0, my_clz_loop
 
 clz_finish:
     mv a0, t1
     ret
-
-# hamming distance function
-hamming_distance:
-    addi sp, sp, -24
-    xor x2, x1, x2
-
-# main function
-
 
 main_func:
     la a0, print_string
@@ -59,5 +73,6 @@ main_func:
 
     lw a1, 0(sp)
     
-
+end_call:
+    ecall
 
